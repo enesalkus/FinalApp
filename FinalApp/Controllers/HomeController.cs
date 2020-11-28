2585ea541603de5a219316d91a7f160ce23b8e97
Enes Alkuş <enesalkus@hotmail.com>
2020-11-28 12:22:29 +0300

    Updt

---
+++ FinalApp/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FinalApp.Models;
+
+namespace FinalApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public static string userName = "Ziyaretçi";
+        public static string[] kategoriList = { "Oturma Odası", "Yatak Odası", "Yemek Odası", "Genç",
+            "Bebek", "Bahçe", "Home Office", "Aydınlatma", "Ev Dekor",
+        "Mutfak"};
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
